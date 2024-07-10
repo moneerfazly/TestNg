b6371dd7f30f7b1a59c63c50feea07e71dbc1b11
Moneer <moneer.fazly@gmail.com>
2024-07-09 23:31:05 -0700

    new class

---
+++ src/test/java/AlertsTest/TesingClass.java
@@ -0,0 +1,16 @@
+package AlertsTest;
+
+import com.google.gson.internal.bind.util.ISO8601Utils;
+import org.testng.annotations.Test;
+
+public class TesingClass {
+
+    @Test
+    public void logInDetails(){
+        System.out.println("Please login into your account");
+    }
+    @Test
+    public void logOutDetails(){
+        System.out.println("Please lgoout as soon as you are done!!!");
+    }
+}
